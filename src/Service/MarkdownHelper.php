8e33cc6bf2af7f6b1f66ce4e065016bad72bbfab
Shorayn <34161708+Shorayn@users.noreply.github.com>
2022-01-01 21:05:32 +0100

    added own configs to services.yml so they can be used inside that specified service

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -9,15 +9,22 @@ class MarkdownHelper {
 
     private $markdownParser;
     private $cache;
+    private $isDebug;
     
-    public function __construct(MarkdownParserInterface $markdownParser, CacheInterface $cache)
+    public function __construct(MarkdownParserInterface $markdownParser, CacheInterface $cache, bool $isDebug)
     {
         $this->markdownParser = $markdownParser;
         $this->cache = $cache;
+        $this->isDebug = $isDebug;
+        dump($isDebug);
     }
 
 
     public function parse (string $src) : string {
+
+        if($this->isDebug){
+            return $this->markdownParser->transformMarkdown($src);
+        }
 
         return $this->cache->get('markdown_'.md5($src), function() use ($src) {
             return $this->markdownParser->transformMarkdown($src);
